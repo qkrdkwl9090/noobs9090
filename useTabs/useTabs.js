04a1a8a70601db016125bf4497ac3d0ab0e8e984
qkrdkwl9090 <qkrdkwl9090@naver.com>
2023-12-07 16:17:19 +0900

    feat(useTabs): publish package

--- useTabs/useTabs.js
+++ useTabs/useTabs.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 export const useTabs = (initialTab, allTabs) => {
   if (!allTabs || !Array.isArray(allTabs)) return;
   const [currentIndex, setCurrentIndex] = useState(initialTab);
